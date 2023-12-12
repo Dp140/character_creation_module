d9756174c662159291fe4d9b8a76336fa44d1e2a
Dp140 <d1401488@yandex.ru>
2023-12-12 20:23:01 +0300

    code according to PEP8

--- test_module.py
+++ test_module.py
@@ -11,3 +11,27 @@ def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
+
+
+
+#special
+
+#if char_class == 'healer':
+        #return (
+            #f'{char_name} применил специальное умение'
+            #f'«Защита {10 + 30}»'
+            #)
+
+#defence
+
+#if char_class == 'healer':
+        #return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+
+
+#attack
+
+#if char_class == 'healer':
+        #return (
+            #f'{char_name} нанёс урон противнику'
+            #f'равный {5 + randint(-3, -1)}'
+            #)
